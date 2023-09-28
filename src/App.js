67524893bca424bce7052d55b8d5bcaed640ee2b
Himanshu Tiwari <himanshu554466@gmail.com>
2023-09-28 19:29:56 +0530

    started to working on this project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Temp from './components/temp';
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      </Temp>
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/temp.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./css/style.css"
+
+const Temp()=>{
+    return(
+        <>
+        <div className="box">
+        <div>
+        <input type="text"
+               className="input-field"
+               /> 
+        </div>
+        </div>
+
+        </>
+    )
+}
+
+export default Temp;
